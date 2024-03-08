fece4529add7492239bb527adff1796e3f510508
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2024-03-09 00:31:27 +0600

    Init: Route and all components create

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import Profile from './Pages/Profile'
+import Register from './Pages/Register'
+import NotFound from './Pages/NotFound'
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Routes>
+        <Route element={<Home />} path='/' exact />
+        <Route element={<Login/>} path='login'/>
+        <Route element={<Profile/>} path='me'/>
+        <Route element={<Register/>} path='register'/>
+        <Route element={<NotFound/>} path='*'/>
+      </Routes>
     </>
   )
 }
---
+++ src/Pages/Register.jsx
@@ -0,0 +1,12 @@
+
+const Register = () => {
+    return (
+        <div>
+            <h2>
+            Register
+            </h2>
+        </div>
+    );
+};
+
+export default Register;
